3c2df43466645e1a1696340d0c1595adde1c26db
BZIvanov <ivanov.biser@gmx.com>
2020-06-12 08:16:38 +0300

    initial commit

---
+++ src/components/pages/home/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Mobile Phones</h1>
+          <span className="subtitle">Shop now</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Tablets</h1>
+          <span className="subtitle">Shop now</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Smart Watches</h1>
+          <span className="subtitle">Shop now</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Fitnes Wrist</h1>
+          <span className="subtitle">Shop now</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Headphones</h1>
+          <span className="subtitle">Shop now</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
