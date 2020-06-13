25d8e37dd2a2d1897b9812b283e11d654101aa65
BZIvanov <ivanov.biser@gmx.com>
2020-06-13 09:35:50 +0300

    categories hover styles

--- src/components/molecules/menu-item/index.jsx
+++ src/components/molecules/menu-item/index.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Typography } from '../../atoms';
+import { Typography, BackgroundImageBox } from '../../atoms';
 
 const ItemCss = styled.article`
-  background-image: url(${({ imageURL }) => imageURL});
-  background-position: center;
-  background-size: cover;
   min-width: 30%;
   height: ${({ size }) => (size === 'large' ? '24rem' : '15rem')};
   flex: 1 1 auto;
@@ -14,10 +11,18 @@ const ItemCss = styled.article`
   justify-content: center;
   border: 1px solid black;
   margin: 0 0.5rem 1rem;
+  transform: scale(1);
+  overflow: hidden;
+  &:hover {
+    cursor: pointer;
+  }
+  & > *:first-child:hover {
+    transform: scale(1.1);
+    transition: transform 5s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+  }
 `;
 
 const ContentCss = styled.div`
-  height: 5.5rem;
   padding: 0 1.5rem;
   display: flex;
   flex-direction: column;
@@ -25,6 +30,9 @@ const ContentCss = styled.div`
   justify-content: center;
   border: 1px solid black;
   margin: 0 0.5rem;
+  background-color: ${({ theme }) => theme.palette.offWhite};
+  position: absolute;
+  opacity: 0.7;
   & *:first-child {
     margin-bottom: 0.5rem;
   }
@@ -36,6 +44,7 @@ const ContentCss = styled.div`
 const MenuItem = (props) => {
   return (
     <ItemCss {...props}>
+      <BackgroundImageBox {...props} />
       <ContentCss>
         <Typography variant="h4">{props.title}</Typography>
         <Typography variant="subtitle1">Shop now</Typography>
