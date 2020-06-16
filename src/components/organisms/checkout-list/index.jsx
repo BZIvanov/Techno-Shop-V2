022e7e6cd10cc6ad1f21669b9238f5fa3ccf2c48
BZIvanov <ivanov.biser@gmx.com>
2020-06-16 23:42:27 +0300

    stripe setup

--- src/components/organisms/checkout-list/index.jsx
+++ src/components/organisms/checkout-list/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { CheckoutProducts } from '../';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import { CheckoutProducts, CheckoutForm } from '../';
 import { CheckoutHeader } from '../../molecules';
 import { AppWidth, Typography } from '../../atoms';
+
+const stripePromise = loadStripe(
+  'pk_test_51GujnyCZFv8tfDD71yeHESG1gByN4TbsrStYrLmUSwZr4KiO6qe1IdTOUvF96Z667jFn0ameJRbvJIvBcvmabcya00K7BZ0lPN'
+);
 
 const CheckoutList = ({ totalPrice }) => {
   return (
@@ -10,6 +16,9 @@ const CheckoutList = ({ totalPrice }) => {
       <CheckoutHeader />
       <CheckoutProducts />
       <Typography variant="h3">TOTAL: {totalPrice.toFixed(2)}lv.</Typography>
+      <Elements stripe={stripePromise}>
+        <CheckoutForm />
+      </Elements>
     </AppWidth>
   );
 };
