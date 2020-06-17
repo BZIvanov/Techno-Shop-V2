d6059fc65910a7594e0a0c5599ce35306397671e
BZIvanov <biser.z.ivanov@gmail.com>
2020-06-17 22:14:56 +0300

    redux thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const middlewares = [];
+const middlewares = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
