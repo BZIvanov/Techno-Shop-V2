5c863810334828a24daefc7777b6a0869072818e
BZIvanov <ivanov.biser@gmx.com>
2020-06-18 07:35:01 +0300

    register action

--- src/store/reducers/user.js
+++ src/store/reducers/user.js
@@ -1,15 +1,35 @@
-import { SET_CURRENT_USER } from '../actions/types';
+import {
+  FETCH_USER_START,
+  FETCH_USER_SUCCESS,
+  FETCH_USER_FAILURE,
+} from '../actions/types';
 
 const INITIAL_STATE = {
+  loading: false,
   user: null,
+  errorMessage: undefined,
 };
 
 const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case SET_CURRENT_USER:
+    case FETCH_USER_START:
       return {
         ...state,
+        loading: true,
+      };
+    case FETCH_USER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
         user: action.payload,
+        errorMessage: undefined,
+      };
+    case FETCH_USER_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        user: null,
+        errorMessage: action.payload,
       };
     default:
       return state;
