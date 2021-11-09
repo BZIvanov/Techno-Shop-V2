bb28f964ff802ff314c80632dc842bc0901c6948
BZIvanov <ivanov.biser@gmx.com>
2021-11-09 11:17:48 +0200

    authenticate and authorize middlewares

--- backend/controllers/rooms.js
+++ backend/controllers/rooms.js
@@ -1,6 +1,7 @@
 const status = require('http-status');
 const Room = require('../models/room');
 const catchAsync = require('../middlewares/catch-async');
+const AppError = require('../utils/app-error');
 
 exports.getAllRooms = catchAsync(async (req, res) => {
   const rooms = await Room.find();
@@ -12,4 +13,46 @@ exports.getAllRooms = catchAsync(async (req, res) => {
     totalCount,
     rooms,
   });
+});
+
+exports.getRoom = catchAsync(async (req, res, next) => {
+  const room = await Room.findById(req.query.id);
+
+  if (!room) {
+    return next(new AppError('Room not found', status.NOT_FOUND));
+  }
+
+  res.status(status.OK).json({ success: true, room });
+});
+
+exports.createRoom = catchAsync(async (req, res) => {
+  const room = await Room.create(req.body);
+
+  res.status(status.CREATED).json({ success: true, room });
+});
+
+exports.updateRoom = catchAsync(async (req, res, next) => {
+  const room = await Room.findById(req.params.id);
+  if (!room) {
+    return next(new AppError('Room not found', status.NOT_FOUND));
+  }
+
+  const updatedRoom = await Room.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(status.OK).json({ success: true, room: updatedRoom });
+});
+
+exports.deleteRoom = catchAsync(async (req, res, next) => {
+  const room = await Room.findById(req.params.id);
+  if (!room) {
+    return next(new AppError('Room not found', status.NOT_FOUND));
+  }
+
+  await room.remove();
+
+  // TODO check response
+  res.status(status.NO_CONTENT).json({});
 });
--- backend/routes/rooms.js
+++ backend/routes/rooms.js
@@ -1,6 +1,25 @@
 const router = require('express').Router();
-const { getAllRooms } = require('../controllers/rooms');
+const {
+  getAllRooms,
+  getRoom,
+  createRoom,
+  updateRoom,
+  deleteRoom,
+} = require('../controllers/rooms');
+const authenticate = require('../middlewares/authenticate');
+const authorize = require('../middlewares/authorize');
+const {
+  userTypes: { admin },
+} = require('../constants');
 
-router.route('/').get(getAllRooms);
+router
+  .route('/')
+  .get(getAllRooms)
+  .post(authenticate, authorize(admin), createRoom);
+router
+  .route('/:id')
+  .get(getRoom)
+  .put(authenticate, authorize(admin), updateRoom)
+  .delete(authenticate, authorize(admin), deleteRoom);
 
 module.exports = router;
