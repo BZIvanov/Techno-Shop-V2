41c930c1f60eaef2fd7f1875168ec403f0168578
BZIvanov <biser.z.ivanov@gmail.com>
2021-11-09 11:17:48 +0200

    authenticate and authorize middlewares

---
+++ backend/middlewares/authenticate.js
@@ -0,0 +1,29 @@
+const status = require('http-status');
+const jwt = require('jsonwebtoken');
+const catchAsync = require('./catch-async');
+const AppError = require('../utils/app-error');
+const User = require('../models/user');
+
+module.exports = catchAsync(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer ')
+  ) {
+    [, token] = req.headers.authorization.split(' ');
+  }
+
+  if (!token) {
+    return next(new AppError('You are not logged in', status.UNAUTHORIZED));
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(new AppError('User not found', status.UNAUTHORIZED));
+  }
+
+  req.user = currentUser;
+  next();
+});
