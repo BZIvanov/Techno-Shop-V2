84be5f1f9b38b4d9c0c9210dd2e750de2899050c
BZIvanov <ivanov.biser@gmx.com>
2021-11-10 22:50:29 +0200

    header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
-import './App.css';
+import { Box } from '@mui/material';
+import { Header } from './components/header';
 
-function App() {
-  return <div>Works</div>;
-}
+const App = () => {
+  return (
+    <Box>
+      <Header />
+    </Box>
+  );
+};
 
 export default App;
