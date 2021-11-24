cce892d62d080369f7c322ddb3b96c5b09fc7d82
BZIvanov <ivanov.biser@gmx.com>
2021-11-25 00:17:19 +0200

    refactor reset and update password with rhf

---
+++ frontend/src/components/user-password-update/form-schema.js
@@ -0,0 +1,15 @@
+import * as yup from 'yup';
+
+const schema = yup
+  .object({
+    oldPassword: yup.string().min(8).required('Old Password is required'),
+    newPassword: yup.string().min(8).required('New Password is required'),
+    confirmNewPassword: yup
+      .string()
+      .min(8)
+      .required('Confirm New Password is required')
+      .oneOf([yup.ref('newPassword'), null], 'Passwords should match'),
+  })
+  .required();
+
+export default schema;
---
+++ frontend/src/pages/reset-password-page/form-schema.js
@@ -0,0 +1,14 @@
+import * as yup from 'yup';
+
+const schema = yup
+  .object({
+    newPassword: yup.string().min(8).required('Password is required'),
+    confirmNewPassword: yup
+      .string()
+      .min(8)
+      .required('Confirm Password is required')
+      .oneOf([yup.ref('newPassword'), null], 'Passwords should match'),
+  })
+  .required();
+
+export default schema;
