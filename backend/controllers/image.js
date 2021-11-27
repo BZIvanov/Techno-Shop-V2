34ad0f373b1fe27d2fd33edfd9c8bf551812d373
BZIvanov <biser.z.ivanov@gmail.com>
2021-11-27 17:08:06 +0200

    upload and remove image endpoints

---
+++ backend/controllers/image.js
@@ -0,0 +1,34 @@
+const status = require('http-status');
+const cloudinary = require('cloudinary');
+const { v4: uuidv4 } = require('uuid');
+const catchAsync = require('../middlewares/catch-async');
+const AppError = require('../utils/app-error');
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+exports.uploadImages = catchAsync(async (req, res) => {
+  const image = await cloudinary.uploader.upload(req.body.image, {
+    public_id: uuidv4(),
+    resource_type: 'auto', // jpeg, png
+  });
+
+  res.status(status.OK).json({
+    success: true,
+    public_id: image.public_id,
+    url: image.secure_url,
+  });
+});
+
+exports.removeImage = catchAsync(async (req, res, next) => {
+  const { result } = await cloudinary.uploader.destroy(req.body.public_id);
+
+  if (result !== 'ok') {
+    return next(new AppError('Remove image error', status.BAD_REQUEST));
+  }
+
+  res.status(status.NO_CONTENT).json();
+});
--- backend/startup/express.js
+++ backend/startup/express.js
@@ -4,6 +4,7 @@ const user = require('../routes/user');
 const category = require('../routes/category');
 const subcategory = require('../routes/subcategory');
 const product = require('../routes/product');
+const image = require('../routes/image');
 const globalError = require('../middlewares/global-error');
 
 module.exports = function startApp(app) {
@@ -15,6 +16,7 @@ module.exports = function startApp(app) {
   app.use('/api/v1/category', category);
   app.use('/api/v1/subcategory', subcategory);
   app.use('/api/v1/product', product);
+  app.use('/api/v1/image', image);
   // globalError has to be the last route
   app.use(globalError);
 };
