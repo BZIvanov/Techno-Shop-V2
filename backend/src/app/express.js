02dfb3b4f07791314dce0a4b207621895ad91d5a
BZIvanov <ivanov.biser@gmx.com>
2022-11-06 20:39:19 +0200

    create src folder

---
+++ backend/src/app/express.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const user = require('../../routes/user');
+const category = require('../../routes/category');
+const subcategory = require('../../routes/subcategory');
+const product = require('../../routes/product');
+const image = require('../../routes/image');
+const globalError = require('../../middlewares/global-error');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: '5mb' }));
+
+app.use('/api/v1/user', user);
+app.use('/api/v1/category', category);
+app.use('/api/v1/subcategory', subcategory);
+app.use('/api/v1/product', product);
+app.use('/api/v1/image', image);
+// globalError has to be the last route
+app.use(globalError);
+
+module.exports = app;
