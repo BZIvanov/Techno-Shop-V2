4cbbe27e11c85d624155b8ad2226da0115a5505b
BZIvanov <biser.z.ivanov@gmail.com>
2022-12-03 15:42:11 +0200

    Update and delete category tests

--- frontend/src/components/category/ManageCategory/ManageCategory.test.jsx
+++ frontend/src/components/category/ManageCategory/ManageCategory.test.jsx
@@ -23,6 +23,21 @@ const handlers = [
       ctx.delay(100)
     );
   }),
+  rest.put(`${process.env.REACT_APP_API}/categories/123`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        success: true,
+        category: { _id: '123', name: 'Boots', slug: 'boots' },
+      }),
+      ctx.delay(100)
+    );
+  }),
+  rest.delete(
+    `${process.env.REACT_APP_API}/categories/123`,
+    (req, res, ctx) => {
+      return res(ctx.json(), ctx.delay(100));
+    }
+  ),
 ];
 
 const server = setupServer(...handlers);
@@ -60,12 +75,22 @@ describe('ManageCategory component', () => {
     test('should display error message if category name is too short', async () => {
       render(<ManageCategory />);
 
-      const categoryNameField = screen.getByRole('textbox', {
-        name: /category name/i,
+      const user = userEvent.setup();
+
+      let categoryNameField;
+      await waitFor(async () => {
+        categoryNameField = screen.getByRole('textbox', {
+          name: /category name/i,
+        });
       });
-      await userEvent.type(categoryNameField, 'a');
+
+      await waitFor(async () => {
+        await user.type(categoryNameField, 'a');
+      });
+
       const createButton = screen.getByRole('button', { name: /create/i });
-      await userEvent.click(createButton);
+      await user.click(createButton);
+
       await waitFor(() => {
         screen.getByText(/category must be at least 2 characters/i);
       });
@@ -85,6 +110,131 @@ describe('ManageCategory component', () => {
         screen.getByRole('button', { name: /Jackets/i });
         const successAlert = screen.getByRole('alert');
         within(successAlert).getByText(/category 'Jackets' created/i);
+      });
+    });
+  });
+
+  describe('Update category', () => {
+    test('should display the updated category name', async () => {
+      server.use(
+        rest.get(`${process.env.REACT_APP_API}/categories`, (req, res, ctx) => {
+          return res(
+            ctx.json({
+              success: true,
+              categories: [
+                { _id: '123', name: 'Shoes', slug: 'shoes' },
+                { _id: '124', name: 'Laptops', slug: 'laptops' },
+              ],
+            }),
+            ctx.delay(100)
+          );
+        })
+      );
+
+      server.use(
+        rest.put(
+          `${process.env.REACT_APP_API}/categories/123`,
+          (req, res, ctx) => {
+            return res(
+              ctx.json({
+                success: true,
+                category: { _id: '123', name: 'Boots', slug: 'boots' },
+              }),
+              ctx.delay(100)
+            );
+          }
+        )
+      );
+
+      render(<ManageCategory />);
+
+      const user = userEvent.setup();
+
+      let categoryChip;
+      await waitFor(() => {
+        categoryChip = screen.getByRole('button', { name: 'Shoes' });
+      });
+      await user.click(categoryChip);
+
+      const categoryNameField = screen.getByRole('textbox', {
+        name: /category name/i,
+      });
+      await userEvent.type(categoryNameField, 'Boots');
+
+      const updateButton = screen.getByRole('button', { name: /update/i });
+      await userEvent.click(updateButton);
+
+      await waitFor(() => {
+        screen.getByRole('button', { name: 'Boots' });
+        const successAlert = screen.getByRole('alert');
+        within(successAlert).getByText(/category 'Boots' updated/i);
+      });
+    });
+  });
+
+  describe('Remove category', () => {
+    test('remove category dialog is displayed after remove chip button is clicked', async () => {
+      const preloadedState = {
+        category: {
+          categories: [{ _id: '123', name: 'Shoes', slug: 'shoes' }],
+        },
+      };
+
+      render(<ManageCategory />, { preloadedState });
+
+      const user = userEvent.setup();
+
+      const categoryChip = screen.getByRole('button', { name: 'Shoes' });
+      const xButton = within(categoryChip).getByTestId('CancelIcon');
+      await user.click(xButton);
+
+      await waitFor(() => {
+        screen.getByText(/are you sure you want to delete this category\?/i);
+      });
+    });
+
+    test('removed category should not be displayed', async () => {
+      server.use(
+        rest.get(`${process.env.REACT_APP_API}/categories`, (req, res, ctx) => {
+          return res(
+            ctx.json({
+              success: true,
+              categories: [
+                { _id: '123', name: 'Shoes', slug: 'shoes' },
+                { _id: '124', name: 'Laptops', slug: 'laptops' },
+              ],
+            }),
+            ctx.delay(100)
+          );
+        })
+      );
+
+      render(<ManageCategory />);
+
+      const user = userEvent.setup();
+
+      let categoryChip;
+      await waitFor(() => {
+        categoryChip = screen.getByRole('button', { name: 'Shoes' });
+      });
+      const xButton = within(categoryChip).getByTestId('CancelIcon');
+      await user.click(xButton);
+
+      let confirmRemoveButton;
+      await waitFor(() => {
+        confirmRemoveButton = screen.getByRole('button', { name: /continue/i });
+      });
+      await user.click(confirmRemoveButton);
+
+      await waitFor(() => {
+        screen.getByRole('button', { name: 'Laptops' });
+        const removedCategoryChip = screen.queryByRole('button', {
+          name: 'Shoes',
+        });
+        expect(removedCategoryChip).not.toBeInTheDocument();
+
+        const successAlert = screen.getByRole('alert');
+        within(successAlert).getByText(/category deleted/i);
       });
     });
   });
