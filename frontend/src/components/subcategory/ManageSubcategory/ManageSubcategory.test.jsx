faabc1f17b27362cc0054df61b573610384b0594
BZIvanov <ivanov.biser@gmx.com>
2022-12-04 11:37:53 +0200

    remove unnecessary useselector

--- frontend/src/components/subcategory/ManageSubcategory/ManageSubcategory.test.jsx
+++ frontend/src/components/subcategory/ManageSubcategory/ManageSubcategory.test.jsx
@@ -1,9 +1,44 @@
-import { render, screen } from '../../../utils/test-utils';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { render, screen, within } from '../../../utils/test-utils';
 import ManageSubcategory from './ManageSubcategory';
 
+// set the api calls which will be called after the component renders initially
+const handlers = [
+  rest.get(`${process.env.REACT_APP_API}/categories`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        success: true,
+        categories: [
+          { _id: '123', name: 'Shoes', slug: 'shoes' },
+          { _id: '124', name: 'Laptops', slug: 'laptops' },
+        ],
+      }),
+      ctx.delay(100)
+    );
+  }),
+  rest.get(`${process.env.REACT_APP_API}/subcategories`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        success: true,
+        subcategories: [],
+      }),
+      ctx.delay(100)
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+
 describe('ManageSubcategory component', () => {
-  describe('Title element', () => {
-    test('render the title with correct text and styles', () => {
+  beforeAll(() => server.listen());
+
+  afterEach(() => server.resetHandlers());
+
+  afterAll(() => server.close());
+
+  describe('Component elements', () => {
+    test('render components elemets successfully', () => {
       render(<ManageSubcategory />);
 
       const titleElement = screen.getByRole('heading', {
@@ -13,6 +48,17 @@ describe('ManageSubcategory component', () => {
         'font-family': 'Roboto,Helvetica,sans-serif',
         'font-weight': '400',
       });
+
+      screen.getByTestId('categoryId');
+      screen.getByRole('button', { name: /reset/i });
+      screen.getByRole('button', { name: /create/i });
+
+      screen.getByRole('textbox', { name: /search/i });
+
+      const subcategoriesList = screen.getByRole('list');
+      within(subcategoriesList).getByText(
+        'No subcategories. Use the form above to create some.'
+      );
     });
   });
 });
