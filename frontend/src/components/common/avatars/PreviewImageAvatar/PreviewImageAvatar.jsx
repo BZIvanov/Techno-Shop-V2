6301198ee36d5f8f55a642316399606252a0dc6b
BZIvanov <biser.z.ivanov@gmail.com>
2022-12-08 11:22:16 +0200

    avatar preview update

---
+++ frontend/src/components/common/avatars/PreviewImageAvatar/PreviewImageAvatar.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import { Badge, Avatar } from '@mui/material';
+import { CloseOutlined } from '../../../mui/Icons';
+
+const PreviewImageAvatar = ({ image, handleRemoveImage }) => {
+  const [preview, setPreview] = useState();
+
+  useEffect(() => {
+    const previewUrl = URL.createObjectURL(image);
+    setPreview(previewUrl);
+
+    return () => {
+      URL.revokeObjectURL(previewUrl);
+    };
+  }, [image]);
+
+  return (
+    <Badge
+      badgeContent={
+        <CloseOutlined
+          sx={{ cursor: 'pointer' }}
+          htmlColor={'red'}
+          onClick={() => {
+            URL.revokeObjectURL(preview);
+            handleRemoveImage(image.name);
+          }}
+        />
+      }
+    >
+      <Avatar
+        alt='product preview'
+        src={preview}
+        sx={{ width: 90, height: 90 }}
+      />
+    </Badge>
+  );
+};
+
+export default PreviewImageAvatar;
