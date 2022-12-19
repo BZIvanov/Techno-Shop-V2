12dde420eb1ec000564247a9c41a51d835d3327a
BZIvanov <ivanov.biser@gmx.com>
2022-12-19 18:27:13 +0200

    add skeleton for the product card

--- frontend/src/components/product/ProductCard/ProductCard.jsx
+++ frontend/src/components/product/ProductCard/ProductCard.jsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
   Typography,
@@ -6,7 +7,9 @@ import {
   CardMedia,
   CardActionArea,
   CardActions,
+  Skeleton,
 } from '@mui/material';
+import { useSelector } from '../../../store/hooks';
 import productImage from '../../../assets/images/product.png';
 
 const ProductCard = ({
@@ -15,28 +18,76 @@ const ProductCard = ({
 }) => {
   const navigate = useNavigate();
 
+  const { loading } = useSelector((state) => state.apiCall);
+
   return (
     <Card>
       <CardActionArea onClick={() => navigate(`/product/${_id}`)}>
-        <CardMedia
-          component='img'
-          height='140'
-          image={images.length > 0 ? images[0].imageUrl : productImage}
-          alt='app product'
-        />
+        {loading ? (
+          <Skeleton
+            sx={{ height: 140 }}
+            animation='wave'
+            variant='rectangular'
+          />
+        ) : (
+          <CardMedia
+            component='img'
+            height='140'
+            image={images.length > 0 ? images[0].imageUrl : productImage}
+            alt='app product'
+          />
+        )}
         <CardContent sx={{ paddingBlock: '8px' }}>
-          <Typography gutterBottom={true} variant='body1'>
-            {title}
-          </Typography>
-          <Typography variant='body2' color='text.secondary'>
-            {description.length > 80
-              ? description.substring(0, 80) + '...'
-              : description}
-          </Typography>
+          {loading ? (
+            <Fragment>
+              <Skeleton
+                animation='wave'
+                height={10}
+                style={{ marginBottom: 6 }}
+              />
+              <Skeleton
+                animation='wave'
+                height={10}
+                width='80%'
+                style={{ marginBottom: 6 }}
+              />
+              <Skeleton animation='wave' height={10} />
+            </Fragment>
+          ) : (
+            <Fragment>
+              <Typography gutterBottom={true} variant='body1'>
+                {title}
+              </Typography>
+              <Typography variant='body2' color='text.secondary'>
+                {description.length > 80
+                  ? description.substring(0, 80) + '...'
+                  : description}
+              </Typography>
+            </Fragment>
+          )}
         </CardContent>
       </CardActionArea>
 
-      <CardActions>{children}</CardActions>
+      <CardActions>
+        {loading ? (
+          <Fragment>
+            <Skeleton
+              animation='wave'
+              variant='circular'
+              width={40}
+              height={40}
+            />
+            <Skeleton
+              animation='wave'
+              variant='circular'
+              width={40}
+              height={40}
+            />
+          </Fragment>
+        ) : (
+          children
+        )}
+      </CardActions>
     </Card>
   );
 };
