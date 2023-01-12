f90c6c5947795ec49b77f9631a43c93ab9981866
BZIvanov <ivanov.biser@gmx.com>
2023-01-12 10:09:27 +0200

    add coupon field

--- frontend/src/components/checkout/Checkout/form-schema.js
+++ frontend/src/components/checkout/Checkout/form-schema.js
@@ -2,12 +2,14 @@ import * as yup from 'yup';
 
 const schema = yup
   .object({
-    address: yup.string().max(500).required(),
+    address: yup.string().max(200).required(),
+    coupon: yup.string().min(2).max(20),
   })
   .required();
 
 const defaultValues = {
   address: '',
+  coupon: '',
 };
 
 export const formConfig = { schema, defaultValues };
