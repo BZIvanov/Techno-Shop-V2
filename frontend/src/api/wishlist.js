2bf0c82d8a2a03f680b6b90e0f1f6b871772e906
BZIvanov <biser.z.ivanov@gmail.com>
2023-02-01 20:09:58 +0200

    wishlists tests

--- frontend/src/api/wishlist.js
+++ frontend/src/api/wishlist.js
@@ -1,14 +1,14 @@
 import axios from './axios';
 
 export const getWishlistCall = (token) => {
-  return axios.get('/wishlist', {
+  return axios.get('/wishlists', {
     headers: { authorization: `Bearer ${token}` },
   });
 };
 
 export const addToWishlistCall = (productId, token) => {
   return axios.post(
-    `/wishlist/${productId}`,
+    `/wishlists/${productId}`,
     {},
     { headers: { authorization: `Bearer ${token}` } }
   );
