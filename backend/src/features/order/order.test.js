fc64221986d8e917e0f96895e7fb62554b63a1bd
BZIvanov <biser.z.ivanov@gmail.com>
2023-02-02 10:14:44 +0200

    orders tests

--- backend/src/features/order/order.test.js
+++ backend/src/features/order/order.test.js
@@ -26,6 +26,18 @@ describe('Order routes', () => {
   });
 
   describe('Get orders controller', () => {
+    test('admin should be get access to all the orders', async () => {
+      const response = await request(app)
+        .get('/v1/orders')
+        .set('Authorization', `Bearer ${signJwtToken(users[0]._id)}`)
+        .expect('Content-Type', /application\/json/)
+        .expect(200);
+
+      expect(response.body).toHaveProperty('success', true);
+      expect(response.body).toHaveProperty('totalCount', 3);
+      expect(response.body).toHaveProperty('orders');
+    });
+
     test('it should return user orders successfully', async () => {
       const response = await request(app)
         .get('/v1/orders')
@@ -34,8 +46,125 @@ describe('Order routes', () => {
         .expect(200);
 
       expect(response.body).toHaveProperty('success', true);
-      expect(response.body).toHaveProperty('totalCount', 1);
+      expect(response.body).toHaveProperty('totalCount', 2);
       expect(response.body).toHaveProperty('orders');
+    });
+  });
+
+  describe('Create order controller', () => {
+    test('it should create an order with correct total price and default status order', async () => {
+      const response = await request(app)
+        .post('/v1/orders')
+        .set('Authorization', `Bearer ${signJwtToken(users[1]._id)}`)
+        .send({
+          cart: [
+            { count: 2, product: products[0]._id },
+            { count: 3, product: products[1]._id },
+          ],
+          address: 'Some test street 23',
+        })
+        .expect('Content-Type', /application\/json/)
+        .expect(201);
+
+      const product1TotalPrice = 2 * products[0].price;
+      const product2TotalPrice = 3 * products[1].price;
+      const totalPrice = product1TotalPrice + product2TotalPrice;
+      expect(response.body).toHaveProperty('success', true);
+      expect(response.body).toHaveProperty('order');
+      expect(response.body.order.totalAmount).toBe(totalPrice);
+      expect(response.body.order.orderStatus).toBe('Not Processed');
+    });
+
+    test('it should create an order with correct total price and deducted coupon', async () => {
+      const response = await request(app)
+        .post('/v1/orders')
+        .set('Authorization', `Bearer ${signJwtToken(users[1]._id)}`)
+        .send({
+          cart: [
+            { count: 2, product: products[0]._id },
+            { count: 3, product: products[1]._id },
+          ],
+          address: 'Some test street 23',
+          coupon: coupons[3].name,
+        })
+        .expect('Content-Type', /application\/json/)
+        .expect(201);
+
+      const product1TotalPrice = 2 * products[0].price;
+      const product2TotalPrice = 3 * products[1].price;
+      const couponDiscountPercent = coupons[3].discount / 100;
+      const totalPrice = product1TotalPrice + product2TotalPrice;
+      const priceWithDiscount = totalPrice - totalPrice * couponDiscountPercent;
+      expect(response.body).toHaveProperty('success', true);
+      expect(response.body).toHaveProperty('order');
+      expect(response.body.order.totalAmount).toBe(priceWithDiscount);
+    });
+
+    test('it should reduce the product quantity and increase sold value for the ordered product', async () => {
+      const orderCount = 5;
+      await request(app)
+        .post('/v1/orders')
+        .set('Authorization', `Bearer ${signJwtToken(users[2]._id)}`)
+        .send({
+          cart: [{ count: orderCount, product: products[12]._id }],
+          address: 'Some test street 23',
+        })
+        .expect('Content-Type', /application\/json/)
+        .expect(201);
+
+      const product = await Product.findById(products[12]._id);
+
+      const updatedProductQuantity = products[12].quantity - orderCount;
+      expect(product.quantity).toBe(updatedProductQuantity);
+      const updatedProductSold = products[12].sold + orderCount;
+      expect(product.sold).toBe(updatedProductSold);
+    });
+
+    test('it should return an error for insuficient product quantity', async () => {
+      const response = await request(app)
+        .post('/v1/orders')
+        .set('Authorization', `Bearer ${signJwtToken(users[1]._id)}`)
+        .send({
+          cart: [{ count: products[1].quantity + 1, product: products[1]._id }],
+          address: 'Some test street 23',
+        })
+        .expect('Content-Type', /application\/json/)
+        .expect(400);
+
+      expect(response.body).toHaveProperty('success', false);
+      expect(response.body).toHaveProperty(
+        'error',
+        'Insufficient product quantity'
+      );
+    });
+  });
+
+  describe('Update order status controller', () => {
+    test('it should update users order successfully if admin', async () => {
+      const response = await request(app)
+        .put(`/v1/orders/${orders[0]._id}`)
+        .set('Authorization', `Bearer ${signJwtToken(users[0]._id)}`)
+        .send({ orderStatus: 'Completed' })
+        .expect('Content-Type', /application\/json/)
+        .expect(200);
+
+      expect(response.body).toHaveProperty('success', true);
+      expect(response.body).toHaveProperty('order.orderStatus', 'Completed');
+    });
+
+    test('it should return an error if the user is not admin', async () => {
+      const response = await request(app)
+        .put(`/v1/orders/${orders[0]._id}`)
+        .set('Authorization', `Bearer ${signJwtToken(users[1]._id)}`)
+        .send({ orderStatus: 'Cancelled' })
+        .expect('Content-Type', /application\/json/)
+        .expect(403);
+
+      expect(response.body).toHaveProperty('success', false);
+      expect(response.body).toHaveProperty(
+        'error',
+        'User is not authorized to access this route'
+      );
     });
   });
 });
