8b789d2082094e70ed9563c3c7a19fe6389a4b12
BZIvanov <ivanov.biser@gmx.com>
2023-12-11 20:36:46 +0200

    cleanup

--- src/components/mui/Icons.jsx
+++
@@ -1,4 +0,0 @@
-import { Home, AddBusiness } from '@mui/icons-material';
-
-export const AddBusinessIcon = (props) => <AddBusiness {...props} />;
-export const HomeIcon = (props) => <Home {...props} />;
